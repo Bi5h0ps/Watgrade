1d12de83dbf2e669aeb0afc952aeccdb17f4ade7
Theo <theo2333lol@gmail.com>
2018-08-26 20:37:35 -0400

    this is an update from mac

--- app/src/main/java/com/example/raiden/test1/add_course.java
+++ app/src/main/java/com/example/raiden/test1/add_course.java
@@ -13,7 +13,7 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class add_course extends AppCompatActivity {
-
+//???
     @BindView(R.id.custom_navi_title)
     TextView tvTitle;
 
